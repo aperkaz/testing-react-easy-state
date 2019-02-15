7e3011c25acb131454a98c37ee9518b6a5b98d7d
Alain Perkaz <alain.perkaz@fortnox.se>
2019-02-15 11:34:59 +0100

    Clean up application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { view } from 'react-easy-state';
+import clock from './store';
 import './App.css';
+
+const Timer = view(() => (
+  <div style={{margin: "5em"}}>{clock.timer}s</div>
+));
+
+setInterval(() => (clock.timer++), 1000);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Timer/>
       </div>
     );
   }
